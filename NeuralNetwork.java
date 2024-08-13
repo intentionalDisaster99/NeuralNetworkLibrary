ee9f1aadc041456632216dd9b74b7e5d4c058f74
IntentionalDisaster99 <abyssalflerken99@gmail.com>
2024-08-13 13:08:25 -0500

    Added mapping

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -85,5 +85,29 @@ public class NeuralNetwork {
     }
 
     // This is the guessing function, called feed forward because you are feeding data forwards through the network
+    public double[] feedForward(double[] inputArr) { 
+    
+        // Making a matrix out of the input
+        Matrix input = new Matrix(inputArr);
+
+        // Getting the first of the hidden outputs because it uses the input layer outputs instead of other hidden outputs
+        Matrix hiddenOutput = Matrix.matrixMult(this.weights_ih, input).add(this.biases_h[0]).map(this.activation);
+
+        // Getting the values of the hidden layer sandwich (All of the hidden layers)
+        for (let i = 1; i < this.hiddenLayout.length; i++) {
+
+            // I don't actually need to save these for the feedforward function, so these aren't going in an array
+            hiddenOutput = Matrix.mult(this.weights_h[i - 1], hiddenOutput).add(this.biases_h[i]).map(this.activation);
+
+        }
+
+        // Generating the output layer values     
+        let output = Matrix.mult(this.weights_ho, hiddenOutput).add(this.bias_o).map(this.activation);
+
+        return output.toArray();
+
+    }
+
+    // The activation function (or )
 
 }
